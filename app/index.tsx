59d18831055c111bb27aed84c6bf9e5ac8dff41e
Omar <omarguaba54@gmail.com>
2025-05-28 12:37:49 -0400

    starting login design

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import Login from "@/components/Login";
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      
+        
+      }}
+    >
+
+    <Login />   
+    </View>
+  );
+}
